c15569cc43d731268770b47058f81a8bf582b2c2
hossain <hossain258@gmail.com>
2022-09-07 10:52:23 +0100

    model update

--- e-ShopAPI-main/App_UserProfile/models.py
+++ e-ShopAPI-main/App_UserProfile/models.py
@@ -7,10 +7,10 @@ class UserProfile(models.Model):
     username =models.OneToOneField(User,on_delete=models.CASCADE)
     phone =models.CharField(max_length=20,blank=False)
     email=models.EmailField(max_length=200,blank=True)
-    addressline1=models.CharField(max_length=200,blank=True)
-    addressline2=models.CharField(max_length=200,blank=True)
+    address=models.CharField(max_length=300,blank=True)    
     city=models.CharField(max_length=200,blank=True)
     country=models.CharField(max_length=200,blank=True)
+    is_varified=models.BooleanField(default=False)
     
     
     def __str__(self):
